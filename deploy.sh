ad4f0728ab5fc488150ba080e462e1edcb356cd3
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-05-14 10:09:03 +0200

    Take the ssh key directly from the environment.

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,10 @@ umask 0077
 
 ansible_dir="/nixos_deploy"
 keyfile="/root/.id_ec"
-vaultpass="/root/.vault_pass"
 hostfile="${ansible_dir}/hosts.yml"
 connection_timeout=120
 
-echo "${VAULT_PASS}" > "${vaultpass}"
-ansible-vault view --vault-password-file="${vaultpass}" "${ansible_dir}"/id_ec_robot.secret > "${keyfile}"
+echo "${NIXOS_ROBOT_KEY}" > "${keyfile}"
 chmod 0400 "${keyfile}"
 
 python3 "${ansible_dir}"/build_inventory.py --keyfile "${keyfile}" \
@@ -23,7 +21,6 @@ export ANSIBLE_HOST_KEY_CHECKING="False"
 export ANSIBLE_SSH_RETRIES=5
 ansible-playbook --timeout="${connection_timeout}" \
                  --key-file "${keyfile}" \
-                 --vault-password-file "${vaultpass}" \
                  --inventory "${hostfile}" \
                  "${ansible_dir}"/deploy.yml
 

a6f94d23ef97b300d5dc22e0feb3785062a4953a
Ibrahim Balla <ibrahim.balla@brussels.msf.org>
2024-04-03 09:39:29 +0200

    Add Nix-Shell to test

--- build_inventory.py
+++ build_inventory.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python3
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash --packages python3
 
 import argparse
 import itertools
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
-#! /usr/bin/env bash
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash
 
 output="$(nix develop '.#default' --command ./build_inventory.py \
             --sshrelay_domain sshrelay.ocb.msf.org \

0fe64cf0f511320294e3918ba970d59d117ba46c
Ibrahim Balla <ibrahim.balla@brussels.msf.org>
2024-04-03 09:44:44 +0200

    Restor bash to test

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash
+#! /usr/bin/env bash
 
 output="$(nix develop '.#default' --command ./build_inventory.py \
             --sshrelay_domain sshrelay.ocb.msf.org \
